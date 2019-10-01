de38f272e2e86840bc568a9837b4150b70137316
Md. Nazmul Huda <nazmulhd10@gmail.com>
2019-10-01 21:40:35 +0600

    Added some file

--- LibraryManagementAPI/Startup.cs
+++ LibraryManagementAPI/Startup.cs
@@ -27,7 +27,7 @@ namespace LibraryManagementAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<LibraryContext>
+            services.AddDbContext<LibraryContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
             services.AddControllers();
         }
 
